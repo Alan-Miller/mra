1d53510a3e10feb38fbbedbe125714f34aa0d293
Alan-Miller <alan.ashman@gmail.com>
2017-12-27 16:22:00 -0700

    edits

--- src/StarWarsData.js
+++ src/StarWarsData.js
@@ -1,32 +1,35 @@
 import React from 'react';
-// import axios from 'axios';
+import axios from 'axios';
 import { setPeople } from './redux/reducer';
 import { connect } from 'react-redux';
 
 class StarWarsData extends React.Component {
 
   componentDidMount() {
+    this.props.history.push('/')
     // AXIOS
-    // axios.get('https://swapi.co/api/people')
-    //   .then(people => {
-    //     // console.log(people.data.results);
-    //     this.props.setPeople(people.data.results); 
-    //   });
+    axios.get('https://swapi.co/api/people')
+    // axios.get('https://requestb.in/18j5yw81')
+      .then(people => {
+        console.log(people);
+        this.props.setPeople(people.data.results); 
+      });
 
     // XHR
-    // const xhr = new XMLHttpRequest();
-    // xhr.open('GET', 'https://swapi.co/api/people');
-    // xhr.onload = _ => { this.props.setPeople(JSON.parse(xhr.response).results); };
-    // xhr.send();
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://swapi.co/api/people');
+    xhr.onload = _ => { this.props.setPeople(JSON.parse(xhr.response).results); };
+    xhr.send();
     
     // FETCH
-    fetch('https://swapi.co/api/people')
-      .then(people => people.json())
-      .then(people => this.props.setPeople(people.results));
+    // fetch('https://swapi.co/api/people')
+    //   // .then(people => {window.aam = people;})
+    //   .then(people => people.json())
+    //   .then(people => this.props.setPeople(people.results));
   }
 
   render() {
-    // console.log('HISTORY', this.props.history)
+    console.log('HISTORY', this.props.history)
     return (
       <div>
         <p>This component gets SW data and puts it on Redux.</p>
